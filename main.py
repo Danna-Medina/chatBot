bc2c4b95bb5ef5bd497d7e5ff834f5245e114490
Danna Medina <danna.medina2869@alumnos.udg.mx>
2023-02-23 16:41:02 -0600

    version2.1

--- main.py
+++ main.py
@@ -40,20 +40,42 @@ def send_message(event=None):
 
 # crear ventana de chat
 root = tk.Tk()
-root.title("Chatbot")
+root.title("Friendchat")
+root.configure(bg='white')
 
-# crear cuadro de chat
-chat_log = tk.Text(root, height=20, width=50, state=tk.DISABLED)
-chat_log.grid(row=0, column=0, padx=10, pady=10, sticky="w")
+# Crear cuadro de chat
+chat_frame = tk.LabelFrame(root, text=" Chat ", font=("Cambria", 12))
+chat_frame.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
 
-# crear campo de entrada
-input_field = tk.Entry(root, width=50)
+chat_log = tk.Text(chat_frame, height=20, width=50, state=tk.DISABLED, font=("Cambria", 12))
+chat_log.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
+
+scrollbar = tk.Scrollbar(chat_frame, command=chat_log.yview)
+scrollbar.grid(row=0, column=1, padx=5, pady=5, sticky="nse")
+
+chat_log.config(yscrollcommand=scrollbar.set)
+
+# Crear campo de entrada
+input_frame = tk.LabelFrame(root, text=" Escribir mensaje ", font=("Cambria", 12))
+input_frame.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")
+
+input_field = tk.Entry(input_frame, width=50, font=("Cambria", 12))
 input_field.bind("<Return>", send_message)
-input_field.grid(row=1, column=0, padx=10, pady=10, sticky="w")
+input_field.pack(padx=10, pady=10, ipady=5, fill=tk.X, expand=True)
 
-# crear botón de envío
-send_button = tk.Button(root, text="Enviar", command=send_message)
-send_button.grid(row=1, column=1, padx=10, pady=10, sticky="e")
+# Crear botón de envío
+send_button = tk.Button(input_frame, text="Enviar", command=send_message, font=("Cambria", 12), bg='#4E4C67')
+send_button.pack(padx=10, pady=10, ipadx=10, ipady=5)
+
+input_field.focus()
+
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+chat_frame.columnconfigure(0, weight=1)
+chat_frame.rowconfigure(0, weight=1)
+
+input_frame.columnconfigure(0, weight=1)
 
 # iniciar aplicación
 root.mainloop()
